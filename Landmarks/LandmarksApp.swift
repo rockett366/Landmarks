3dda9a6c57d8d3da9d0ba9c26e7d5a4c4d476ee8
Rockett <eemejia366@gmail.com>
2024-05-29 17:56:21 -0700

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Sarah Mejia on 5/29/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
